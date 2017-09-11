49a0c7c8d809cc89dd077cb4d03aa8128631cd69
iasilva <@Estevao2210>
2017-09-11 11:58:30 -0300

    Exibição por tags melhorada e consultas

--- App/View/Admin/home.php
+++ App/View/Admin/home.php
@@ -1,13 +1,19 @@
 
 <?php include_once HTML_DIR.DS.'admin-head.php'; ?>
 <?php include_once HTML_DIR.DS.'admin-header.php'; ?>
+<div class="container-fluid barra-top-notice-blue">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="container">
+                <h1> Bem vindo <span style="opacity: .5;"><?php echo $user =(isset($_SESSION['user']))?$_SESSION['user']['name']:''; ?></span>  <span class="glyphicon glyphicon-home" style="font-size: 16px; opacity: .5;"></span> </h1>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 <main class="container">
-    
-    
-    <header>
-        <h3>Home admin renderizado</h3>
-    </header>
+
     
     
     
